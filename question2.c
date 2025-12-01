e8511c48efeecbed6fcf49e05bf9ef59576e2b61
cullen-thompson <cullen.thompson@tcu.edu>
2025-12-01 17:09:59 -0600

    Question 2 done

---
+++ question2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int max3(int a, int b, int c) {
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max;
+}
+
+int main() {
+    int a,b,c;
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a,&b,&c);
+    printf("Max: %d\n", max3(a,b,c));
+}
+
